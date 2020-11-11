d4890c6f44c94bafdc1873fad3c2d47e3a76f1e7
Kishorkumar.K <kishorkumar.kumaran@gmail.com>
2020-11-11 19:25:40 +0530

    solution for module 4
    added solution for module 4 of "html,css,javascript for web developers" course from coursera

---
+++ module4-solution/script.js
@@ -0,0 +1,15 @@
+
+//Solution of assignment 4:
+
+
+(function() {
+    var names = ["Yaakov", "John", "Jen", "Paul", "Frank", "Larry", "Jason", "Paula", "Laura", "Jim"];
+    for (var i = 0; i < names.length; i++) {
+        var firstLetter = names[i].charAt(0).toLowerCase();
+        if (firstLetter === 'j') {
+            byeSpeaker.speak(names[i]);
+        } else {
+            helloSpeaker.speak(names[i]);
+        }
+    }
+})();
